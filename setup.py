300cc52bb1af583f47e4ec615344d802c1689dfc
Krzysztof Jurewicz <krzysztof.jurewicz@laboratorium.ee>
2012-10-16 17:21:12 +0200

    Fixed name in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='Bootstrap Django Admin Templates',
+setup(name='bootstrap-django-admin',
       version='0.0.1',
       description='Django Admin templates using Twitter Bootstrap',
       url='https://github.com/KrzysiekJ/bootstrap-django-admin',
