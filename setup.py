3f3c3a441a0ecbcdae0acdafc142e3db9740578c
Krzysztof Jurewicz <krzysztof.jurewicz@laboratorium.ee>
2012-10-16 15:22:35 +0200

    Application directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Bootstrap Django Admin Templates',
+      version='0.0.1',
+      description='Django Admin templates using Twitter Bootstrap',
+      url='https://github.com/KrzysiekJ/bootstrap-django-admin',
+      packages=['admin_bootstrap'],
+    )
