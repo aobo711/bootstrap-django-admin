faaae271f462e8007a527da223f45cd9aedb54db
Adam Bogdal <adam@bogdal.pl>
2012-12-21 20:34:39 +0100

    Include static and templates files to the source distibution

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='bootstrap-django-admin',
-      version='0.0.1',
-      description='Django Admin templates using Twitter Bootstrap',
-      url='https://github.com/KrzysiekJ/bootstrap-django-admin',
-      packages=['admin_bootstrap'],
-    )
+setup(
+    name='bootstrap-django-admin',
+    version='0.0.1',
+    description='Django Admin templates using Twitter Bootstrap',
+    url='https://github.com/aobo711/bootstrap-django-admin.git',
+    download_url='https://github.com/aobo711/bootstrap-django-admin/zipball/master',
+    packages=['admin_bootstrap'],
+    include_package_data=True)
